270fd3bee4f88fe90b51aa352c68800b24bf148b
jjubbu <hogugugut4102.off@gmail.com>
2022-01-14 23:52:09 +0900

    Feat: 클릭한 숫자 출력

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import Button from './elements/Button';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  let list = [...Array(9)].map((l,i)=>String(i+1));
+  list.unshift('0');
+  list.unshift('00');
+  list.unshift('.');
+
+  const math = ['+','-','*','%','/']
+
+  const [number, setNumber] = React.useState<string>('')
+
+  const numberClick = (num:string,type:string) => {
+    const len = number.length;
+    console.log(number[len-1])
+    if (type === 'math' && (number.length === 0 || math.includes(number[len-1]))){
+      alert('숫자를 입력해주세요.')
+    }else{
+      setNumber(prev=>prev+num)
+    }
+  }
+
+    return (
+        <div className="App">
+            <h3>정말정말 간단한 계산기</h3>
+            <p>result : {number}</p>
+            {list.map((l,i)=>{
+              return <button key={i} onClick={()=>{numberClick(l,'number')}}>{l}</button>
+            })}
+            <br/>
+            {math.map((l,i)=>{
+              return <button key={i} onClick={()=>{numberClick(l,'math')}}>{l}</button>
+            })}
+            <br/>
+            <button>enter</button>
+        </div>
+    );
 }
 
 export default App;
